1070f65223952041c6a683c923ca241f11df19e3
Ivam <ivam.developer@gmail.com>
2019-10-08 17:21:16 -0300

    First_commit

---
+++ app-test/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/components/header/header.component';
+import { FooterComponent } from './views/components/footer/footer.component';
+import { HomeComponent } from './views/pages/home/home.component';
+import { AboutComponent } from './views/pages/about/about.component';
+import { ContactComponent } from './views/pages/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
