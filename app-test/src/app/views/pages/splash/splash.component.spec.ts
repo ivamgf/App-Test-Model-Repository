cc0e6d85f4fff5adee7b65824a4c8d75b46b8a55
Ivam <ivam.developer@gmail.com>
2019-10-24 21:32:13 -0300

    Tests
    Jasmine

--- app-test/src/app/views/pages/splash/splash.component.spec.ts
+++ app-test/src/app/views/pages/splash/splash.component.spec.ts
@@ -18,8 +18,4 @@ describe('SplashComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
